7bdb6846907ebac41a276c77e2632a069af5a04c
yash kumar <yashkumar.967565@gmail.com>
2025-12-26 16:29:24 +0530

    useEffet Hook using setintervel

--- React-practice/src/App.jsx
+++ React-practice/src/App.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 
 const App = () => {
   return (
@@ -12,23 +12,24 @@ const App = () => {
 }
 function Counter(){
 const [count,setCount] = useState(0)
-  function increaseCount(){
-    setCount(count + 1);
 
-  }
-  function DecreaseCount(){
-    setCount(count-1);
-  }
-
-  function ResetCount(){
-    setCount(0)
-  }
+useEffect(function(){
+  setInterval(function(){
+    setCount(function(count){
+      return count+1;
+    })
+  },1000)
+  console.log("mounted");
+  
+},[]);
+function increaseCount(){
+  setCount(count+1);
+}
 
   return <div>
     <h1 id='text'>{count}</h1>
     <button onClick={increaseCount}>Increase count</button>
-    <button onClick={DecreaseCount}>Decrease count</button>
-    <button onClick={ResetCount}>Reset count</button>
+    
   </div>
   
 }
