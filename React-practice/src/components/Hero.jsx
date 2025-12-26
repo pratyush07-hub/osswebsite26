7824e6f797179f0e2993465af829a55624433c65
yash kumar <yashkumar.967565@gmail.com>
2025-12-26 18:27:21 +0530

    Adding Hellq welcome oss animation

--- React-practice/src/components/Hero.jsx
+++ React-practice/src/components/Hero.jsx
@@ -1,20 +1,45 @@
+
 import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import "../styles/hero.css";
 
 export default function Hero() {
-  const [showText, setShowText] = useState(false);
+  const lines = [
+    "Hello !",
+    "Welcome to OSS",
+    "Fueling Tech Passion Through Open Source"
+  ];
+
+  const [displayedText, setDisplayedText] = useState("");
+  const [lineIndex, setLineIndex] = useState(0); 
+  const [charIndex, setCharIndex] = useState(0); 
 
   useEffect(() => {
-    const timer = setTimeout(() => setShowText(true), 1000);
+    const timer = setTimeout(() => {
+      const currentLine = lines[lineIndex];
+      
+      
+      setDisplayedText(currentLine.slice(0, charIndex + 1));
+      setCharIndex(prev => prev + 1);
+
+      
+      if (charIndex + 1 === currentLine.length) {
+        setTimeout(() => {
+          setCharIndex(0);
+          setLineIndex((lineIndex + 1) % lines.length); 
+          setDisplayedText("");
+        }, 800); 
+      }
+    }, 100);
+
     return () => clearTimeout(timer);
-  }, []);
+  }, [charIndex, lineIndex]);
 
   return (
     <div className="hero">
       <Navbar />
 
-      
+    
       <div className="code-bg">
         <pre>
 {`
@@ -23,72 +48,15 @@ while(true){
   contribute();
 }
 
-function buildOSS (){
-  return "Open Source Society";
-}
-
-for(let i = 0;i < 100; i++){
-  console.log(" Code Running... ");
-}
-  
-
-if(openSource){
-  learn();
-  build();
-  contribute();
-
-  #include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        int a;
-        cin >> a;
-        string str1;
-        cin >> str1;
-        int m, n;
-        cin >> m >> n;
-        vector <long long> v1(n);
-        vector <long long> v2(n);
-        for(int i = 0; i < n; i++) cin >> v1[i];
-        for(int i = 0;i < n; i++) cin >> v2[i];
-         string str2, str3;
-        cin >> str2>> str3;
-        deque <int> dq;
-        for(char ch : str1){
-            dq.push_back(ch);
-        }
-        for(int i = 0;i < m; i++){
-            if(str3[i]=='V'){
-                dq.push_front(str2 [i]);
-            }else{
-            dq.push_back(str2 [i]);
-        }
-        }
-        for(char ch : dq){
-            cout << ch;
-        }
-        cout << endl;
-        
-    }
-}
-
-}
+// ... your repeated code ...
 `.repeat(20)}
         </pre>
       </div>
 
-    
-      {showText && (
-        <div className={`hero-text ${showText ? "show" : ""}`}>
-          <h1>Hello !</h1>
-          <h2>Welcome to OSS</h2>
-          <p>Fueling Tech Passion Through Open Source</p>
-        </div>
-      )}
+
+      <div className="hero-text">
+        <h1>{displayedText}</h1>
+      </div>
     </div>
   );
 }
-
-
