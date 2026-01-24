08cf807d79482547ef61cb90ee87d21bd136b9a7
prakhardixit9793 <prakhardixit9793@gmail.com>
2026-01-24 11:19:29 +0530

    Added AKGEC to About section

--- React-practice/src/App.jsx
+++ React-practice/src/App.jsx
@@ -1,8 +1,16 @@
 import "./styles/global.css";
 import Hero from "./components/Hero";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <Hero />;
+  return(
+    <div>
+    <Navbar />
+    <Outlet/>
+    </div>
+
+  );
 }
 
 export default App;
